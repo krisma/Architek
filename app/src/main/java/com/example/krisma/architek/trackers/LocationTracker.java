1292492aa5018ffbf789e3cb7f13c1d162d67254
Stefan Michael Pedersen <stefanmp91@gmail.com>
2015-07-19 02:38:34 +0200

    Switched to LocationClient

--- app/src/main/java/com/example/krisma/architek/trackers/LocationTracker.java
+++ app/src/main/java/com/example/krisma/architek/trackers/LocationTracker.java
@@ -1,120 +1,78 @@
 package com.example.krisma.architek.trackers;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.LocationSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LocationTracker implements LocationListener, LocationSource {
+public class LocationTracker implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, LocationSource {
 
     private final Context mContext;
-
-    // flag for GPS status
-    boolean isGPSEnabled = false;
-
-    // flag for network status
-    boolean isNetworkEnabled = false;
-
-    // flag for GPS status
-    boolean canGetLocation = false;
-
-    Location location; // location
-    double latitude; // latitude
-    double longitude; // longitude
-
-    // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0;
-
-    // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 0;
+    Location location;
+    double latitude;
+    double longitude;
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
+    private boolean mRequestingLocationUpdates;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     public LocationTracker(Context context) {
         this.mContext = context;
-        getLocation();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mRequestingLocationUpdates = true;
+
+        mGoogleApiClient.connect();
     }
 
-    public Location getLocation() {
-        try {
-            locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-
-            // getting GPS status
-            isGPSEnabled = locationManager
-                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-            // getting network status
-            isNetworkEnabled = locationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // no network provider is enabled
-            } else {
-                this.canGetLocation = true;
-                // First get location from Network Provider
-                if (isNetworkEnabled) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    Log.d("Network", "Network");
-                    if (locationManager != null) {
-                        location = locationManager
-                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                MIN_TIME_BW_UPDATES,
-                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        Log.d("GPS Enabled", "GPS Enabled");
-                        if (locationManager != null) {
-                            location = locationManager
-                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                            }
-                        }
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
         }
-
-        return location;
     }
 
-    /**
-     * Stop using GPS listener
-     * Calling this function will stop using GPS in your app
-     */
-    public void stopUsingGPS() {
-        if (locationManager != null) {
-            locationManager.removeUpdates(LocationTracker.this);
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    private LocationRequest getLocationRequest() {
+        if (mLocationRequest == null) {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.setInterval(2000);
+            mLocationRequest.setFastestInterval(1000);
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         }
+        return mLocationRequest;
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, getLocationRequest(), this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
     /**
@@ -149,17 +107,17 @@ public class LocationTracker implements LocationListener, LocationSource {
 
         this.location = location;
 
-        float accuracyChange = lastLocation.getAccuracy() - location.getAccuracy();
+        if(lastLocation != null && this.location != null) {
+            float accuracyChange = lastLocation.getAccuracy() - location.getAccuracy();
 
-        float acc = location.hasAccuracy() ? location.getAccuracy() : 0;
-        Log.d("GPSTracker", getLatitude() + " : " + getLongitude() + " acc: " + acc + " change:" + accuracyChange);
+            float acc = location.hasAccuracy() ? location.getAccuracy() : 0;
+            Log.d("GPSTracker", getLatitude() + " : " + getLongitude() + " acc: " + acc + " change:" + accuracyChange);
 
-        if (accuracyChange < -10) {
-            Log.w("GPSTracker", "Went indoors - Change to Dead Reckoning!");
-            LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(TRANSITION_ACTION));
+            if (accuracyChange < -10) {
+                Log.w("GPSTracker", "Went indoors - Change to Dead Reckoning!");
+                LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(TRANSITION_ACTION));
+            }
         }
-
-
     }
 
     public static final String TRANSITION_ACTION = "TRANSITION_TO_INDOOR_ACTION";
@@ -168,18 +126,6 @@ public class LocationTracker implements LocationListener, LocationSource {
         for (OnLocationChangedListener listener : listeners) {
             listener.onLocationChanged(location);
         }
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 
     private List<OnLocationChangedListener> listeners = new ArrayList<>();
@@ -202,5 +148,10 @@ public class LocationTracker implements LocationListener, LocationSource {
 
     public Location getCurrentLocation(){
         return location;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
